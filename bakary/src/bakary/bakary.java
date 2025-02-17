5228ae3f1c9d7f495f593e702493125e3b01e04a
halaaab <hala.abdulrahman01@gmail.com>
2025-02-17 22:26:55 +0300

    added all the classes

--- bakary/src/bakary/bakary.java
+++ bakary/src/bakary/bakary.java
@@ -1,11 +1,6 @@
 package bakary;
 
 public class bakary {
-	public static void main(String [] args ) {
-		System.out.println("hala");
-		System.out.println("hala");
-		System.out.println("hala");
-
-	}
+	
 
 }
