2c26cee7122f368a8ab47b024c082a187695ebe9
halaaab <hala.abdulrahman01@gmail.com>
2025-02-17 22:23:04 +0300

    added print

--- bakary/src/bakary/bakary.java
+++ bakary/src/bakary/bakary.java
@@ -3,6 +3,9 @@ package bakary;
 public class bakary {
 	public static void main(String [] args ) {
 		System.out.println("hala");
+		System.out.println("hala");
+		System.out.println("hala");
+
 	}
 
 }
